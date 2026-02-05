1e57767fe18856727fd776a90cba9638ffb9cf00
unknown <nobelleon.86@gmail.com>
2026-02-05 14:24:38 +0700

    adding vercel

--- LEATHERWATCHES/settings.py
+++ LEATHERWATCHES/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+q=pa$iddnhl&986n3_#p12y@$2^#t2#)@*rc#ea35r0zc#e)c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000']
+ALLOWED_HOSTS = []
 
 
 # Application definition
