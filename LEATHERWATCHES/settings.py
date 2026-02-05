3eff9ca251a98d49b81435f0db144a9fea964710
unknown <nobelleon.86@gmail.com>
2026-02-05 22:59:53 +0700

    adding vercel

--- LEATHERWATCHES/settings.py
+++ LEATHERWATCHES/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-+q=pa$iddnhl&986n3_#p12y@$2^#t2#)@*rc#ea35r0zc#e)c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1:8000', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -115,7 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'leatherwatchesapp/static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'leatherwatchesapp/static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'leatherwatchesapp/static')
+
+MEDIA_URL = "img/"
+MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
